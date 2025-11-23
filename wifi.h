ca4f3adb9efcb2750f67cfada29d6441a5d6383b
andris <andris@z-b.lv>
2025-11-23 12:15:49 +0200

    feat: added wifi connection, +refactor to separate funcionality

---
+++ wifi.h
@@ -0,0 +1,17 @@
+// example of arrudino wifi 
+#include <WiFiS3.h>
+
+#ifndef WIFI_H
+#define WIFI_H
+
+#define WIFI_SERVER_PORT  80
+
+extern int status;     // the WiFi radio's status
+extern WiFiServer server;           //WiFiServer
+
+void printWifiData();
+void printCurrentNet();
+void printMacAddress(byte mac[]);
+void connectToWifi(int *status, const char * ssid, const char * pass);
+
+#endif
