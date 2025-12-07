02e34b934c75cf7e5e7cbe5bb41c385e8151edb1
andris <andris@z-b.lv>
2025-12-07 22:14:54 +0200

    feat: introduced README, minor adjustment for code quality

--- heat-exporter.h
+++ heat-exporter.h
@@ -4,6 +4,26 @@
 
 
 
+
+#define DHTPIN1 2
+#define DHTPIN2 3
+#define DHTPIN3 4
+#define DHTPIN4 5
+#define DHTPIN5 6
+#define DHT_CONECTED 5
+#define DHT_DELAY_SENSOR 0
+//#define DHT_USE_HUMIDITY
+#define BUTTON_PIN 8
+#define LED_PIN 7
+
 #define DHT_INTERVAL 2000
 #define BUTTON_INTERVAL 250
+#define CLIENT_INTERVAL 50
+#define MIN_BUTTON_INTERVAL 50
+
+
+#define WIFI_SERVER_PORT 80
+
+
+#define DHTTYPE DHT22
 
